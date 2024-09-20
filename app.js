0a62a56130ca87f7b699c9dc5017dcb9a8e626ff
Thiago Giacomoni <giacomoni201820@gmail.com>
2024-09-20 18:41:58 -0300

    alterando o limite para 10

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector("p");
 // paragrafo.innerHTML = "Escolha um número entre 1 e 20.";
 let listaDeNumerosSorteados = [];
-let numeroLimite = 20;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto!');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20.');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10.');
 }
 
 exibirMensagemInicial();
